57055bf310bd6a76d55d4b7bcb72444a8b12015d
Vincent Chan <your.email@example.com>
2025-05-27 17:34:01 +0800

    Reverted to full Next.js application instead of static export

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    unoptimized: true,
-  },
-  output: 'export',
-  distDir: 'out',
-  // For a custom domain
-  // basePath: '/tig-technology',
-  // For GitHub Pages
-  // assetPrefix: '/tig-technology',
+  }
 }
 
 module.exports = nextConfig;
